3bf162c7f9e5ffa9a6fad528115ad9d4bcafaa32
sebas119 <lopez.sebastian@correounivalle.edu.co>
2019-08-14 23:23:54 -0500

    Extendiendo el modelo usuario para agregar una tabla de roles

--- apps/administrador/models.py
+++ apps/administrador/models.py
@@ -1,100 +1,29 @@
 # admin.models.py
 
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
+from model_utils.models import TimeStampedModel
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
+class Role(TimeStampedModel):
 
-        user = self.model(
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_staffuser(self, email, password):
-        """
-        Creates and saves a staff user with the given email and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.staff = True
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password):
-        """
-        Creates and saves a superuser with the given email and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.staff = True
-        user.admin = True
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser):    
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
+    USER_TYPE_CHOICES = (
+        (1, 'administrador'),
+        (2, 'cliente_online'),
+        (3, 'vendedor_tpv'),
     )
-    active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False)  # a admin user; non super-user
-    admin = models.BooleanField(default=False)  # a superuser
-    # notice the absence of a "Password field", that's built in.
-    object = UserManager()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.PositiveSmallIntegerField(
+        choices=USER_TYPE_CHOICES, primary_key=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []  # Email & Password are required by default.
 
-    def get_full_name(self):
-        # The user is identified by their email address
-        return self.email
+@receiver(post_save, sender=User)
+def create_user_role(sender, instance, created, **kwargs):
+    if created:
+            Rolees.objects.create(user=instance)
 
-    def get_short_name(self):
-        # The user is identified by their email address
-        return self.email
-
-    def __str__(self):              # __unicode__ on Python 2
-        return self.email
-
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        return self.staff
-
-    @property
-    def is_admin(self):
-        "Is the user a admin member?"
-        return self.admin
-
-    @property
-    def is_active(self):
-        "Is the user active?"
-        return self.active
+@receiver(post_save, sender=User)
+def create_user_role(sender, instance, **kwargs):
+    instance.role.save()
