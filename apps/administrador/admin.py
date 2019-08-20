3ce7db0d6fd93dfb25d967c9a1007980b7e90bfe
sebas119Laptop <lopez.sebastian@correounivalle.edu.co>
2019-08-20 12:06:04 -0500

    Modelos actualizados

--- apps/administrador/admin.py
+++ apps/administrador/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 from .models import Role
+from .models import Item, OrderItem, Order
 
-# Register your models here.
+
+# Admin models
 admin.site.register(Role)
+
+# Products
+
+admin.site.register(Item)
+admin.site.register(OrderItem)
+admin.site.register(Order)
